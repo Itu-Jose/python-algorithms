edb69149fc68511c7318a21e1ce755ceb4b5d327
Itu-Jose <itujose653@gmail.com>
2023-05-06 14:43:38 +0200

    Add bubble sort

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import sorting_algorithms as sa
+import sorting_algorithms as sort
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     ls = [5,6,8,10,1,5,13]
-    sort = sa.Sort()
-    print(sort.selection(arr=ls))
+    # print(sort.minimum(ls))
+    print(sort.bubble_optimized(arr=ls))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,26 +1,76 @@
-class Sort:
+def minimum(arr: list[int]) -> int:
+    """
+    Find minimum integer in list
+    :param arr:
+    :return: minimum integer in arr
+    """
+    min_val = float('inf')
+    for i in range(len(arr)):
+        if arr[i] < min_val:
+            min_val = arr[i]
 
-    def minimum(self, arr: list[int]) -> int:
-        min_val = float('inf')
-        for i in range(len(arr)):
-            if arr[i] < min_val:
-                min_val = arr[i]
+    return min_val
 
-        return min_val
+def maximum(arr: list[int]) -> int:
+    """
+    Find maximum integer in list
+    :param arr:
+    :return: maximum integer in arr
+    """
 
-    def maximum(self, arr: list[int]) -> int:
-        max_val = float('-inf')
-        for i in range(len(arr)):
-            if arr[i] > max_val:
-                max_val = arr[i]
+    max_val = float('-inf')
+    for i in range(len(arr)):
+        if arr[i] > max_val:
+            max_val = arr[i]
 
-        return max_val
+    return max_val
 
-    def selection(self, arr: list[int]) -> list[int]:
-        for i in range(len(arr)):
-            arr_min = self.minimum(arr[i::])
-            if arr[i] > arr_min:
-                j = arr[i::].index(arr_min)
-                arr[i], arr[j] = arr[j], arr[i]
+def selection(arr: list[int]) -> list[int]:
+    """
+    Performs a selection sort on an input list. Takes the smallest/largest integer
+    from the unsorted portion of the list and moves it to the sorted portion of the
+    list.
+    :param arr:
+    :return: sorted list
+    """
+    for i in range(len(arr)):
+        arr_min = minimum(arr[i+1::])
+        if arr[i] > arr_min:
+            j = arr[i+1::].index(arr_min) + i + 1
+            arr[i], arr[j] = arr[j], arr[i]
 
-        return arr
+    return arr
+
+def bubble(arr: list[int]) -> list[int]:
+    """
+    Performs a bubble sort on an input list. Repeatedly swaps adjacent elements
+    if they are in the wrong order.
+    :param arr:
+    :return: sorted list
+    """
+    n = len(arr)
+    for j in range(n):
+        for i in range(n-j-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+
+    return arr
+
+def bubble_optimized(arr: list[int]) -> list[int]:
+    """
+    Performs a bubble sort on an input list. Stops if inner loop
+    swap does not occur.
+    :param arr:
+    :return: sorted list
+    """
+    n = len(arr)
+    for j in range(n):
+        swap = False
+        for i in range(n-j-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swap = True
+        if not swap:
+            break
+
+    return arr
